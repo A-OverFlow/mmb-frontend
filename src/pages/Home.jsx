afa74a9c371097fd7c0e4c319f575602b78f71e2
devyyj <devyyj@gmail.com>
2025-03-19 08:15:38 +0900

    초기 페이지 완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import {useSelector} from "react-redux";
-import {Box, Typography} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {Box, Button, Typography} from "@mui/material";
+import {alert} from "../slices/alertSlice.js";
 
 const Home = () => {
+  const dispatch = useDispatch();
   const accessToken = useSelector((state) => state.auth.accessToken);
 
   return (
@@ -22,6 +24,36 @@ const Home = () => {
         <Typography variant="body1">
           {accessToken ? '로그인 완료!' : '로그인 하지 않았습니다.'}
         </Typography>
+
+        {/* 알림 테스트 버튼 추가 */}
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => dispatch(alert.success("성공 메시지!"))}
+        >
+          성공 알림
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => dispatch(alert.error("에러 발생!"))}
+        >
+          에러 알림
+        </Button>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => dispatch(alert.info("정보 메시지"))}
+        >
+          정보 알림
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={() => dispatch(alert.warning("경고 메시지!"))}
+        >
+          경고 알림
+        </Button>
       </Box>
     </Box>
   );
